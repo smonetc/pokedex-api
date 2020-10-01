caeb9297972ef736faedff67a18d1694089453e6
Monet Cooper <smomoc@monets-mbp-2.lan>
2020-10-01 19:32:31 -0400

    removed code

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(cors())
 app.use(function validateBearerToken(req, res, next) {
     const apiToken = process.env.API_TOKEN
     const authToken = req.get('Authorization')
-    console.log(authToken)
+
     if (!authToken || authToken.split(' ')[1] !== apiToken) {
       return res.status(401).json({ error: 'Unauthorized request' })
     }
