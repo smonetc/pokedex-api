90493c5618c19aea1e5be5659a8d77b3b49c11ec
Monet Cooper <smomoc@monets-mbp-2.lan>
2020-10-01 18:50:51 -0400

    final edits

--- server.js
+++ server.js
@@ -1,21 +1,28 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const POKEDEX = require('./pokedex.json')
 
-console.log(process.env.API_TOKEN)
 
 const app = express()
 
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
+app.use(helmet())
+app.use(cors())
 
-function validateBearerToken(req, res, next) {
-    console.log('validate bearer token middleware')
-    debugger
-     // move to the next middleware
-     next()
-}
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+    console.log(authToken)
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+      return res.status(401).json({ error: 'Unauthorized request' })
+    }
 
-app.use(validateBearerToken)
+    next()
+  })
 
 const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
 
@@ -25,13 +32,42 @@ function handleGetTypes(req, res) {
     
 app.get('/types', handleGetTypes)
 
-function handleGetPokemon(req, res) {
-    res.send('Hello, Pokemon!')
-}
+app.get('/types', function handleGetTypes(req, res) {
+    res.json(validTypes)
+  })
+  
+app.get('/pokemon', function handleGetPokemon(req, res) {
+    let response = POKEDEX.pokemon;
+  
+   
+    if (req.query.name) {
+      response = response.filter(pokemon =>
+        // case insensitive searching
+        pokemon.name.toLowerCase().includes(req.query.name.toLowerCase())
+      )
+    }
+  
     
-app.get('/pokemon', handleGetPokemon)
+    if (req.query.type) {
+      response = response.filter(pokemon =>
+        pokemon.type.includes(req.query.type)
+      )
+    }
+  
+    res.json(response)
+  })
 
-const PORT = 8000
+  app.use((error, req, res, next) => {
+    let response
+    if (process.env.NODE_ENV === 'production') {
+      response = { error: { message: 'server error' }}
+    } else {
+      response = { error }
+    }
+    res.status(500).json(response)
+    })
+
+  const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
